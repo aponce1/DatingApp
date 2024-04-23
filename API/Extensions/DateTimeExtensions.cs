5f0ec1ead00bcc398c2cb06615d45aa99d09a474
unknown <you@example.com>
2024-04-23 13:05:46 -0400

    End of section 8

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateOnly dod)
+        {
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            var age = today.Year - dod.Year;
+
+            if (dod > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+}
