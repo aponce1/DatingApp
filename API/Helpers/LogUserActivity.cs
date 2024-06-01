ec8b2906230bbcc63ed89669c4bca2a953d8bf67
unknown <you@example.com>
2024-06-01 12:03:56 -0400

    End of section 14

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -15,7 +15,7 @@ namespace API.Helpers
             var userId = resultContext.HttpContext.User.GetUserId();
 
             var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
-            var user = await repo.GetByIdAsync(int.Parse(userId));
+            var user = await repo.GetByIdAsync(userId);
             user.LastActive = DateTime.UtcNow;
             await repo.SaveAllAsync();
         }
